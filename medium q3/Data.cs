20ede45649cabf70a67cbe2bd7a5eb9138f40ab6
karthik <karthik.venkata@kanini.com>
2021-01-27 12:33:31 +0530

    Add project files.

---
+++ medium q3/Data.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medium_q1
+{
+    class Data
+    {
+        public static List<Employee> Employees = new List<Employee>();
+        public static void PrintEmployee()
+        {
+            Console.WriteLine("Please enter the employee Name");
+            String key =Console.ReadLine();
+            var emp = (from Employee in Employees where Employee.Name == key select Employee);
+            Console.WriteLine("\nDetails of the employee with name "+key+"\n");
+            foreach (var i in emp)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static void SortAndPrintEmployees()
+        {
+            Employees.RemoveAt(Employees.Count - 1);
+            Data.Employees.Sort();
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("\nEmployee list after sorting \n");
+            Data.Employees.ForEach(employee => Console.WriteLine(employee));
+            Console.WriteLine("-------------------------------------");
+
+
+        }
+
+    }
+}
